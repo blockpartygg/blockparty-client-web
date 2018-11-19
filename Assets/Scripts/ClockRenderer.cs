913207b13d420d6a0307e0e4601539af9304c0a0
Ron Forbes <ron@ronforbes.me>
2018-11-18 23:34:08 -0800

    Removed commented out code from Clock Renderer

--- Assets/Scripts/ClockRenderer.cs
+++ Assets/Scripts/ClockRenderer.cs
@@ -8,17 +8,7 @@ public class ClockRenderer : MonoBehaviour {
 	public TMP_Text ClockText;
 
 	void Update() {
-		// switch(GameManager.Instance.State) {
-		// 	case GameManager.GameState.MinigameStart:
-		// 		ClockText.text = "30<sup>:00</sup>";
-		// 		break;
-		// 	case GameManager.GameState.MinigamePlay:
-		// 		TimeSpan timeRemaining = GameManager.Instance.EndTime > DateTime.Now ? GameManager.Instance.EndTime - DateTime.Now : TimeSpan.Zero;
-		// 		ClockText.text = string.Format("{0}:{1:D2}<sup>:{2:D3}</sup>", timeRemaining.Minutes, timeRemaining.Seconds, timeRemaining.Milliseconds);
-		// 		break;
-		// 	case GameManager.GameState.MinigameEnd:
-		// 		ClockText.text = "00<sup>:00</sup>";
-		// 		break;
-		// }
+		TimeSpan timeRemaining = GameManager.Instance.EndTime > DateTime.Now ? GameManager.Instance.EndTime - DateTime.Now : TimeSpan.Zero;
+		ClockText.text = string.Format("{0}:{1:D2}<sup>:{2:D3}</sup>", timeRemaining.Minutes, timeRemaining.Seconds, timeRemaining.Milliseconds);
 	}
 }

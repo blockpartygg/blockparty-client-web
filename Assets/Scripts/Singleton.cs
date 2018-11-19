0117eb6d694a1f975bcd6be4b80663258a9923f9
Ron Forbes <ron@ronforbes.me>
2018-11-18 23:35:39 -0800

    Imported base Singleton class from previous Unity project

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+	static T instance;
+	public static T Instance {
+		get {
+			if(instance == null) {
+				instance = (T)FindObjectOfType(typeof(T));
+				if(FindObjectsOfType(typeof(T)).Length > 1) {
+					return instance;
+				}
+				if(instance == null) {
+					GameObject singleton = new GameObject();
+					instance = singleton.AddComponent<T>();
+					singleton.name = typeof(T).ToString();
+					DontDestroyOnLoad(singleton);
+				}
+			}
+			return instance;
+		}
+	}
+}
